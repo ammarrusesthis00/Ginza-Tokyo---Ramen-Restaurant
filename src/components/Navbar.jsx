fa448ecc80251d8d9b09453da79d78810acf03ac
Muhammad Ammar <ammarrusesthis00@gmail.com>
2026-03-01 16:19:50 +0500

    Update Navbar styles and CTA text: Added lg: padding for larger screens, Adjusted font weights for links, Changed CTA text from 'Book Online' to 'Order Online', Updated text size for desktop title, Modified gaps between links for mobile and desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Menu } from "lucide-react";
 export default function Navbar() {
     return (
         <>
-            <nav className="text-white flex flex-col py-2 md:py-4 max-w-[90%] mx-auto">
+            <nav className="text-white flex flex-col py-2 md:py-4 lg:py-6">
                 <div id="mobile" className="md:hidden">
                     <div className="flex items-center justify-between">
                         <h2 className="text-xl font-classy">Ginza Tokyo</h2>
@@ -15,16 +15,15 @@ export default function Navbar() {
                     <div className="">
                         <h2 className="text-2xl font-classy">Ginza Tokyo</h2>
                     </div>
-                    <ul className="flex items-center gap-3">
-                        <a href="" className="font-rope font-extralight">Menu</a>
-                        <a href="" className="font-rope font-extralight">About Us</a>
-                        <a href="" className="font-rope font-extralight">Gallery</a>
-                        <a href="" className="font-rope font-extralight">Location</a>
-                        <a href="" className="font-rope font-extralight">FAQs</a>
-                        <a href="" className="font-rope font-extralight">Contact</a>
+                    <ul className="flex items-center gap-4">
+                        <a href="" className="font-rope font-light">About Us</a>
+                        <a href="" className="font-rope font-light">Gallery</a>
+                        <a href="" className="font-rope font-light">Location</a>
+                        <a href="" className="font-rope font-light">FAQs</a>
+                        <a href="" className="font-rope font-light">Contact</a>
                         
                         <a href="" className="bg-black px-3 py-2 font-classy border border-white/30" style = {{textShadow: '0 0 8px white'}}>
-                            Book Online
+                            Order Online
                         </a>
                     </ul>
                     <Menu />
@@ -32,19 +31,19 @@ export default function Navbar() {
 
                 <div id="desktop" className="hidden lg:flex items-center justify-between">
                     <div>
-                        <h2 className="font-classy text-3xl">Ginza Tokyo</h2>
+                        <h2 className="font-classy text-2xl">Ginza Tokyo</h2>
                     </div>
 
                     <ul className="flex items-center gap-3">
                         <div className="hidden md:flex items-center gap-9">
-                            <a href="" className="font-rope font-extralight">Menu</a>
-                            <a href="" className="font-rope font-extralight">About Us</a>
-                            <a href="" className="font-rope font-extralight">Gallery</a>
-                            <a href="" className="font-rope font-extralight">Location</a>
-                            <a href="" className="font-rope font-extralight">Contact</a>
+                            <a href="" className="font-rope font-light">Menu</a>
+                            <a href="" className="font-rope font-light">About Us</a>
+                            <a href="" className="font-rope font-light">Gallery</a>
+                            <a href="" className="font-rope font-light">Location</a>
+                            <a href="" className="font-rope font-light">Contact</a>
 
                             <a href="" className="bg-black px-3 py-2 font-classy border border-white/30" style = {{textShadow: '0 0 8px white'}}>
-                                Book Online
+                                Order Online
                             </a>
                         </div>
                     </ul>
