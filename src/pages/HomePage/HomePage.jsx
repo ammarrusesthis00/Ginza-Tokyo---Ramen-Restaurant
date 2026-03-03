d8bf49c80c8bbe2c492cca7318b7f384b2521886
Muhammad Ammar <ammarrusesthis00@gmail.com>
2026-03-03 06:47:03 +0500

    added import lines for other sections

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,14 @@
 import Hero from "./sections/Hero";
+import Statistics from "./sections/Statistics";
+import Experience from "./sections/Experience";
+import Menu from "./sections/Menu";
 export default function Homepage() {
     return (
         <>
             <Hero />
+            <Statistics />
+            <Experience />
+            <Menu />
         </>
     )
 }
