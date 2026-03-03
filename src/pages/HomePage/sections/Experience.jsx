38b284c776ba7f2c3871a6bdda69d32f13913bc5
Muhammad Ammar <ammarrusesthis00@gmail.com>
2026-03-03 07:48:17 +0500

    changed h4 to h4 and chaned textcontent and changed space-y and alos changed max-w and added bgc,padding,rounded-lg

--- src/pages/HomePage/sections/Experience.jsx
+++ src/pages/HomePage/sections/Experience.jsx
@@ -4,7 +4,7 @@ export default function Experience() {
   const items = [
     {
       title: "Warm Interior",
-      description: "Soft lighting and natural wood tones create a calm atmosphere."
+      description: "Soft lighting create a calm atmosphere."
     },
     {
       title: "Crafted Coffee",
@@ -22,20 +22,20 @@ export default function Experience() {
 
   return (
     <section className="w-[90%] mx-auto max-w-6xl space-y-8 pb-26">
-      <div className="space-y-2">
+      <div className="space-y-1">
         <h2 className="text-white text-2xl md:text-3xl font-classy">The Experience</h2>
         <p className="text-gray-100 font-rope font-light max-w-[90ch]">
           Step inside a space designed for comfort, connection, and carefully crafted moments.
         </p>
       </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 items-center justify-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-2 items-center justify-items-center bg-black p-8 rounded-lg">
             {items.map((item, i) => (
-              <div key={i} className="lg:max-w-[70%] text-center space-y-4 flex flex-col justify-between relative">
+              <div key={i} className="lg:max-w-[90%] text-center space-y-4 flex flex-col justify-between relative">
                   <img src={Image1} alt={item.name} className="w-full rounded-lg" />img
                   <div className="space-y-1 absolute bottom-10 px-2">
-                    <h4 className="text-xl text-white font-classy">{item.title}</h4>
-                    <p className="text-sm text-gray-100 font-rope font-light">
+                    <h3 className="text-xl text-white font-classy">{item.title}</h3>
+                    <p className="text-base text-gray-100 font-rope font-light">
                         {item.description}
                     </p>
                   </div>
